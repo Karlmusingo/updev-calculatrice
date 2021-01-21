84fc17d8d150537195bf13c7c15ab1f07a25a90a
princebalolagebetos <betosbalolage@gmail.com>
2021-01-21 13:55:59 +0100

    design body card

---
+++ index.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Calculatrice</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+      <div id="tableau">
+          <img id="image" src="equal.png" alt="" width="5%">
+
+          <label id="label1">hhhh</label>
+          
+          <label id="label2" for="">dddd</label>
+          
+      </div>   
+  </body>
+</html>
